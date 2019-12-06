2d46f42715767fea799cdd849cbb77c445e19a9d
Ali Rahemi <deutschlandmyhome@gmail.com>
2019-12-06 16:44:36 +0100

    add sass-lint

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is Laravel Application from the Youtube Series</p>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-8 mx-auto">
+                <h4>{{$title}}</h4>
+                <p>This is Laravel Application from the Youtube Series</p>
+            </div>
+        </div>
+    </div>
 @endsection
